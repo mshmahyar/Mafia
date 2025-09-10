23cd681b692c20357258bd9b1dda5b50aea43a59
mshmahyar <mshmahyar9@gmail.com>
2025-09-10 22:21:17 +0330

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if not API_TOKEN:
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot) ,storage=MemoryStorage())
+dp = Dispatcher(bot, storage=storage)
 
 # ======================
 # متغیرهای سراسری
